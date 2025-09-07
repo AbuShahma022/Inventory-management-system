047df7f47f167c282550994e09e2f9ed2dd4aa4b
AbuShahma022 <abushahma1014@gmail.com>
2025-09-07 14:14:38 +0600

    Initial commit: backend folder setup

---
+++ backend/app.js
@@ -0,0 +1,60 @@
+// Basic Lib Import
+import express from "express";
+import router from "./src/routes/api.js"; 
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+// Security Middleware Imports
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import hpp from "hpp";
+import cors from "cors";
+
+// Load environment variables
+dotenv.config();
+// Express App Init
+const app = express();
+// Security Middlewares Implement
+app.use(cors());
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(hpp());
+// Body Parser
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+// Rate Limiter
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 1000,                // limit each IP to 1000 requests per window
+  message: "Too many requests from this IP, please try again later."
+});
+app.use(limiter);
+
+// MongoDB Connection
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, { autoIndex: true });
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error("MongoDB Connection Error:", err);
+    process.exit(1); // Exit if DB connection fails
+  }
+};
+
+connectDB();
+
+
+// Routes
+
+app.use("/api/v1", router);
+
+
+// Handle Undefined Routes
+
+app.use((req, res) => {
+  res.status(404).json({ status: "fail", data: "Not Found" });
+});
+
+// Export App
+export default app;
